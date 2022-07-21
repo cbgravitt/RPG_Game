23ff563ae1344326646f806661e072ca2ef26c98
Charlie Gravitt <cbgravitt@cox.net>
2022-07-20 21:33:57 -0700

    items, enemies, and comments

--- RPG/RPG_Main.py
+++ RPG/RPG_Main.py
@@ -2,6 +2,7 @@ import random as rn
 import RPG_class as c
 from time import sleep
 
+#takes in a name, retuns the new character to play as
 def makeNewCharacter():
     name = str(input("\nEnter your character's name: "))
     return c.Character(name, None)
@@ -9,12 +10,20 @@ def makeNewCharacter():
 def getSave():
     return
 
+#returns a semi-random list of enemies depending on the area. Each area of 
+#the game has an ID, 1 being the starting area and incrementing up in the order
+#they are unlocked. This first version has one area and five enemy types within it.
+def generate_enemies(area):
+    
+    return
+
+#The main gameplay loop!
 def play(hero: c.Character):
     print("You, ", hero.name,", are the hero of our story.", sep="")
     return
 
 def main():
-    print("Welcome to... well I don't have a title yet. Enjoy!")
+    print("Welcome to Ashen Conquest. Enjoy!")
     while(1):
         i = int(input("\nPress 1 to start a new game, or press 2 to load a save file: "))
         if i==1 or i==2:
--- RPG/RPG_class.py
+++ RPG/RPG_class.py
@@ -1,3 +1,6 @@
+import random as rn
+import RPG_items as loot
+
 class Item:
     def __init__(self, name, AD, Def, HP, Equipped, LvL, type, rarity):
         #Name of the item
@@ -25,6 +28,7 @@ class Item:
         self.Equipped = 0
 
 class Character:
+    #Core stats. Wallet will not be used in this version. 
     Wallet = 0
     AD = 5
     Max_HP = 100
@@ -35,14 +39,17 @@ class Character:
     XP = 0
     XP_needed = 100
 
+    #Role to be implemented later
     def __init__(self, name, role):
         self.name = name
         self.role = role
     
+    #For now, XP needs scale linearly with level. May change to exponential in the future.
     def levelUp(self):
         self.Level += 1
         self.XP_needed += self.Level * 100
 
+    #simple fxn for gaining xp. Checks for a level-up.
     def addXP(self, XP):
         if self.XP + XP >= self.XP_needed:
             self.levelUp()
@@ -50,9 +57,13 @@ class Character:
         else:
             self.XP += XP
     
+    #adds an item to the inventory.
     def getItem(self, item: Item):
         self.Inventory.append(item)
 
+    #Equips an item. Has returns for if the item is already equipped or if level is insufficient.
+    #If another item of the same type (ex: weapon, helmet) is already equipped, unequips it and
+    #equips the selected item.
     def equipItem(self, item: Item):
         if item.Equipped == 1:
             print("Item is already equipped!")
@@ -71,13 +82,45 @@ class Character:
             self.Curr_HP += item.HP
         else:
             print("You are not a high enough level to equip this item!")
-    
+
 class Enemy:
-    def __init__(self, name, b_AD, b_Def, b_HP, level, scaling):
+    lootTable = [tuple]
+    def __init__(self, name, b_AD, b_Def, b_HP, level, XP):
         self.name = name
-        self.AD = b_AD + level*scaling
-        self.Def = b_Def + level*scaling
-        self.HP = b_HP + level*scaling
+        self.AD = b_AD * level
+        self.Def = b_Def * level
+        self.HP = b_HP * level
+        #the XP a monster gives out when it dies
+        self.XP = XP * level
+    
+    #every enemy has a Loot Table, a list of items paired with odds. How it works is that 
+    #all the odds values total to 1. When defeated, a random number 0-1 is generated. If it
+    #is greater than lootable[0][0] (the odds of the first item), then the odds of that item
+    #are subtracted from that number and we move on to loottable[1][0] until the random number
+    #is reduced to less than the odds of something. Then, that item is given. 
+    #As an ex, if the three values are [.8, nothing], [.1, bronze sword], [.1, bronze helmet],
+    #rolling below .8 gives nothing. rolling a .8-.899999... gives sword, .9-1 gives helmet.
+    def addLoot(self, item: Item, odds):
+        self.lootTable.append([odds, item])
+
+#in V 0.0.1 (the first version), there are five enemy types. Generator fxns are below. 
+#Each fxn has a short lore comment that I will hopefully expand upon!
+
+#The Surge are the grunts of the Ashen. They are reanimated corpses filled with carefully enchanted
+#electrical magic that pilots the body automatically. Easy to make and unfailingly loyal, they are the footsoldiers
+#of the Ashen King. Their name comes from the fact that, when they die, they occasionally release a small surge of electricity,
+#disorienting nearby Surge and shocking anyone touching them slightly. Clearly, the spell is not perfect...
+def makeSurge(lvl):
+    surge = Enemy("Surge", 7, 10, 48 + rn.randint(-3, 2), lvl, 20)
+    surge.addLoot(None, .90)
+    surge.addLoot(loot.bronze_sword, .05)
+    surge.addLoot(loot.bronze_boots, .01)
+    surge.addLoot(loot.bronze_gauntlets, .01)
+    surge.addLoot(loot.bronze_helmet, .01)
+    surge.addLoot(loot.bronze_chest, .01)
+    surge.addLoot(loot.bronzed_banner, .01)
+
+
 
 
             
